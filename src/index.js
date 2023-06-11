c00b999e2e4640229aae8656b5c2a8121a833ee8
Karthikraj <karthikraj@Apples-MacBook-Pro.local>
2023-06-12 02:12:05 +0530

    firebase deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import http from 'http';
-// import ServerlessHttp from "serverless-http";
 import authRoutes from "./routes/auth.js";
 import tutorRoutes from "./routes/tutors.js";
 import studentRotes from "./routes/student.js";
@@ -43,7 +42,7 @@ app.use(cors());
 // Parse JSON bodies
 app.use(express.json());
 
-app.use(express.static('build'));
+app.use(express.static('dist'));
 
 // Setup database connection
 mongoose
@@ -88,15 +87,12 @@ app.use((err, req, res, next) => {
     .json({ errMsg: err.message || "Something went wrong..." });
 });
 
-app.use('./netlify/functions/api')
-export default app;
-
 // Start server
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 // app.listen(port, () => {
 //   console.log(`Server started on port ${port}`);
 // });
 
-// server.listen(port, () => {
-//   console.log(`Server listening on port ${port}`);
-// });
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
